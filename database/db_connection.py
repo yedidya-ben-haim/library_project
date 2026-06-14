5c2f4ef921b0660a88e2b5e7e1ad4d70ce3af57f
yedidya ben haim <bhyedidya@gmail.com>
2026-06-14 17:13:22 +0300

    Creating a DbConnection class function

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,47 +1,55 @@
 import mysql.connector
 
-def get_connection():
-    """
-        Creates a connection to the database and returns it.
-    """
-    return mysql.connector.connect(user='root',
-                                   password='secret',
-                                   host='127.0.0.1',
-                                   database='library_db')
+
+class DbConnection:
+    def __init__(self):
+        self.user = user='root'
+        self.password='secret'
+        self.host='127.0.0.1'
+        self.database='library_db'
+
+    def get_connection(self):
+        """
+            Creates a connection to the database and returns it.
+        """
+        return mysql.connector.connect(user=self.user,
+                                       password=self.password,
+                                       host=self.host,
+                                       database=self.database)
 
 
-def create_tables():
-    """
-        Creates the 'members' and 'books' tables if they do not exist
-        — runs at server startup, at the beginning of the main function
-    """
-    members_tables_query = """CREATE TABLE IF NOT EXISTS members (
-                                id INT PRIMARY KEY AUTO_INCREMENT,
-                                name VARCHAR(50) NOT NULL,
-                                email VARCHAR(50) UNIQUE NOT NULL,
-                                is_active BOOLEAN NOT NULL DEFAULT TRUE,
-                                total_borrows INT NOT NULL DEFAULT 0
-                                );
-                            """
-
-    books_tables_query = """CREATE TABLE IF NOT EXISTS books (
+    def create_tables(self):
+        """
+            Creates the 'members' and 'books' tables if they do not exist
+            — runs at server startup, at the beginning of the main function
+        """
+        members_tables_query = """CREATE TABLE IF NOT EXISTS members (
                                     id INT PRIMARY KEY AUTO_INCREMENT,
-                                    title VARCHAR(50) NOT NULL,
-                                    author VARCHAR(50) NOT NULL,
-                                    genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other') NOT NULL,
-                                    is_available BOOLEAN NOT NULL DEFAULT TRUE,
-                                    borrowed_by_member_id INT NULL
+                                    name VARCHAR(50) NOT NULL,
+                                    email VARCHAR(50) UNIQUE NOT NULL,
+                                    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+                                    total_borrows INT NOT NULL DEFAULT 0
                                     );
                                 """
 
-    conn = get_connection()
-    cursor = conn.cursor()
+        books_tables_query = """CREATE TABLE IF NOT EXISTS books (
+                                        id INT PRIMARY KEY AUTO_INCREMENT,
+                                        title VARCHAR(50) NOT NULL,
+                                        author VARCHAR(50) NOT NULL,
+                                        genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other') NOT NULL,
+                                        is_available BOOLEAN NOT NULL DEFAULT TRUE,
+                                        borrowed_by_member_id INT NULL
+                                        );
+                                    """
 
-    cursor.execute(members_tables_query)
-    cursor.execute(books_tables_query)
+        conn = self.get_connection()
+        cursor = conn.cursor()
 
-    conn.commit()
+        cursor.execute(members_tables_query)
+        cursor.execute(books_tables_query)
 
-    cursor.close()
-    conn.close()
+        conn.commit()
+
+        cursor.close()
+        conn.close()
 
