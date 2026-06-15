b85fb84ef4b6a39afe63c6942f3ba81a75bb08dc
yedidya ben haim <bhyedidya@gmail.com>
2026-06-15 10:02:16 +0300

    Implementing router.get('/books/{id}')

--- routes/book_routes.py
+++ routes/book_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from database import book_db
 
 new_book = book_db.BookDB()
@@ -10,5 +10,12 @@ router = APIRouter()
 def get_all_books():
     return new_book.get_all_books()
 
-@router.get("/books")
+
+@router.get("/books/{id}")
+def get_book_by_id(id: int):
+    book = new_book.get_book_by_id(id)
+    if book:
+        return book
+    else:
+        raise HTTPException(status_code=404, detail="The book does not exist.")
 
