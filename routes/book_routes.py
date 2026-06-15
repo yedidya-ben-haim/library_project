5b9d7d9065538064419424f826d664cf044360ac
yedidya ben haim <bhyedidya@gmail.com>
2026-06-15 10:56:08 +0300

    Implementing @router.post '/books'

--- routes/book_routes.py
+++ routes/book_routes.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter, HTTPException
 from database import book_db
+from pydantic import BaseModel
+from typing import Literal
 
 new_book = book_db.BookDB()
-
 router = APIRouter()
 
+
+class CreateBook(BaseModel):
+    title: str
+    author: str
+    genre: Literal["Fiction", "Non-Fiction", "Science", "History", "Other"]
 
 @router.get("/books")
 def get_all_books():
@@ -17,5 +23,13 @@ def get_book_by_id(id: int):
     if book:
         return book
     else:
-        raise HTTPException(status_code=404, detail="The book does not exist.")
+        raise HTTPException(status_code=404, detail="Book not found")
+
+
+@router.post("/books", status_code=201)
+def create_book(data: CreateBook):
+    data = data.model_dump()
+    book_created = new_book.create_book(data)
+    if not book_created:
+        raise HTTPException(status_code=400, detail="The book was not created.")
 
